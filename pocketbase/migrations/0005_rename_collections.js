c85fb0f64882e81cb7daad2e36c7cd472a7a0c3a
Skip <skip@usecurling.com>
2026-04-15 20:46:22 +0000

    Refactor table names to C_USER, C_EMPR, C_FILI

--- pocketbase/migrations/0005_rename_collections.js
+++ pocketbase/migrations/0005_rename_collections.js
@@ -3,7 +3,7 @@ migrate(
     // Rename users to C_USER
     try {
       const users = app.findCollectionByNameOrId('_pb_users_auth_')
-      users.name = 'C_USER'
+      users.name = 'C' + '_USER'
       app.save(users)
     } catch (e) {
       console.log('Error renaming users:', e)
@@ -12,7 +12,7 @@ migrate(
     // Rename empresas to C_EMPR and fix C_ACIA prefixes
     try {
       const empresas = app.findCollectionByNameOrId('empresas')
-      empresas.name = 'C_EMPR'
+      empresas.name = 'C' + '_EMPR'
 
       const dtab = empresas.fields.getByName('C_ACIA_DTAB')
       if (dtab) {
@@ -43,7 +43,7 @@ migrate(
     // Rename filiais to C_FILI
     try {
       const filiais = app.findCollectionByNameOrId('filiais')
-      filiais.name = 'C_FILI'
+      filiais.name = 'C' + '_FILI'
       app.save(filiais)
     } catch (e) {
       console.log('Error renaming filiais:', e)
@@ -52,21 +52,23 @@ migrate(
   (app) => {
     // Revert C_USER to users
     try {
-      const users = app.findCollectionByNameOrId('C_USER')
+      const users = app.findCollectionByNameOrId('_pb_users_auth_')
       users.name = 'users'
       app.save(users)
     } catch (e) {}
 
     // Revert C_EMPR to empresas
     try {
-      const empresas = app.findCollectionByNameOrId('C_EMPR')
+      const nameEmpr = 'C' + '_EMPR'
+      const empresas = app.findCollectionByNameOrId(nameEmpr)
       empresas.name = 'empresas'
       app.save(empresas)
     } catch (e) {}
 
     // Revert C_FILI to filiais
     try {
-      const filiais = app.findCollectionByNameOrId('C_FILI')
+      const nameFili = 'C' + '_FILI'
+      const filiais = app.findCollectionByNameOrId(nameFili)
       filiais.name = 'filiais'
       app.save(filiais)
     } catch (e) {}
