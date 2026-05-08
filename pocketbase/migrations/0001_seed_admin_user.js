dbd2ec5873e1c92d71bd39788c92b190d9bd45d5
Skip <skip@usecurling.com>
2026-05-08 16:28:17 +0000

    feat: Implement login page and authentication

--- pocketbase/migrations/0001_seed_admin_user.js
+++ pocketbase/migrations/0001_seed_admin_user.js
@@ -1,34 +1,6 @@
 migrate(
   (app) => {
-    let users
-    try {
-      users = app.findCollectionByNameOrId('_pb_users_auth_')
-    } catch (_) {
-      users = new Collection({
-        id: '_pb_users_auth_',
-        name: 'users',
-        type: 'auth',
-        listRule: 'id = @request.auth.id',
-        viewRule: 'id = @request.auth.id',
-        createRule: '',
-        updateRule: 'id = @request.auth.id',
-        deleteRule: 'id = @request.auth.id',
-        fields: [
-          {
-            name: 'name',
-            type: 'text',
-          },
-          {
-            name: 'avatar',
-            type: 'file',
-            maxSelect: 1,
-            maxSize: 5242880,
-            mimeTypes: ['image/jpeg', 'image/png', 'image/svg+xml', 'image/gif', 'image/webp'],
-          },
-        ],
-      })
-      app.save(users)
-    }
+    const users = app.findCollectionByNameOrId('_pb_users_auth_')
 
     try {
       app.findAuthRecordByEmail('_pb_users_auth_', 'gomesalg@gmail.com')
